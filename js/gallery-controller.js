3eb6eb38e293906a1ac06a8f281fc78406dc772b
Simon <simonifergan239@gmail.com>
2019-02-16 17:22:00 +0200

    mobile with editor mode

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -27,6 +27,7 @@ function onStartEditor(imgId) {
     document.querySelector('#gallery').hidden = true;
     document.querySelector('#about').style.display = 'none';
     document.querySelector('#contact').style.display = 'none';
+    document.querySelector('.main-header').classList.toggle('.editor-mode');
     initMeme(imgId);
 
 }
